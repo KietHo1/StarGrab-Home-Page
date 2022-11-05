aa2606f047a0b18f5e0501eeb0f58cf557297aed
KietHo1 <kiethostartup1999@gmail.com>
2022-11-05 19:40:27 +0700

    ADD Body

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./UI/Header/Header";
 import Footer from "./UI/Footer/Footer";
+import Body from "./UI/Body/Body";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Header />
+        <Body />
         <Footer />
       </header>
     </div>
--- src/UI/Body/Body.js
+++ src/UI/Body/Body.js
@@ -1,6 +1,20 @@
-import "../Body/Body.module.css";
+import classes from "../Body/Body.module.css";
 function Body() {
-  return <div></div>;
+  return (
+    <main>
+      <div className={classes.body}>
+        <div className={classes.bar1}></div>
+        <div className={classes.bar2}></div>
+        <div className={classes.bar3}></div>
+        <div className={classes.bar4}></div>
+        <div className={classes.bar5}></div>
+        <div className={classes.bar6}></div>
+        <div className={classes.bar7}></div>
+        <div className={classes.bar8}></div>
+        <div className={classes.bar9}></div>
+      </div>
+    </main>
+  );
 }
 
 export default Body;
