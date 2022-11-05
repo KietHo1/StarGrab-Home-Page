0aea95972c535055cae8fc7dc707ca9ec68116f6
KietHo1 <kiethostartup1999@gmail.com>
2022-11-05 15:23:22 +0700

    Footer Complete 💝

--- src/UI/Footer/Footer.js
+++ src/UI/Footer/Footer.js
@@ -2,15 +2,19 @@ import classes from "../Footer/Footer.module.css";
 function Footer() {
   return (
     <main>
-      <div className={classes.body}>
+      <div className={classes.footer}>
         <div className={classes.bar1}>
           <div className={classes.News}>
             <div className={classes.titleNews}>Tin tức</div>
+            <br />
             <div>Xe giường nằm Limousine: đỉnh cao mới của ngành xe khách</div>
+            <br />
             <div>
               Xe limousine đi Vũng Tàu: Tổng hợp top 6 xe chất lượng cao
             </div>
+            <br />
             <div>Review xe limousine đi Đà Lạt: những câu hỏi thường gặp</div>
+            <br />
             <div>
               Xe limousine đi Sapa: Tổng hợp top các hãng xe chất lượng cao
             </div>
@@ -18,96 +22,220 @@ function Footer() {
 
           <div className={classes.Route}>
             <div className={classes.titleRoute}>Tuyến đường</div>
+            <br />
             <div>Xe đi Buôn Mê Thuột từ Sài Gòn</div>
+            <br />
             <div>Xe đi Vũng Tàu từ Sài Gòn</div>
+            <br />
             <div>Xe đi Nha Trang từ Sài Gòn</div>
+            <br />
             <div>Xe đi Đà Lạt từ Sài Gòn</div>
+            <br />
             <div>Xe đi Sapa từ Hà Nội</div>
+            <br />
             <div>Xe đi Hải Phòng từ Hà Nội</div>
+            <br />
             <div>Xe đi Vinh từ Hà Nội</div>
           </div>
 
           <div className={classes.Limousine}>
-            <div className={classes.titleLimousine}>Tuyến đường</div>
+            <div className={classes.titleLimousine}>Xe Limousine</div>
+            <br />
             <div>Xe Limousine đi Đà Lạt từ Sài Gòn</div>
+            <br />
             <div>Xe Limousine đi Vũng Tàu từ Sài Gòn</div>
+            <br />
             <div>Xe Limousine đi Nha Trang từ Sài Gòn</div>
+            <br />
             <div>Xe Limousine đi Hải Phòng từ Hà Nội</div>
+            <br />
             <div>Xe Limousine đi Hạ Long từ Hà Nội</div>
+            <br />
             <div>Xe Limousine đi Sapa Từ Hà Nội</div>
+            <br />
             <div>Xe Limousine đi Quảng Ninh từ Hà Nội</div>
           </div>
         </div>
         <div className={classes.bar2}>
           <div className={classes.Station}>
             <div className={classes.titleStation}>Bến xe</div>
+            <br />
             <div>Bến xe Miền Đông</div>
+            <br />
             <div>Bến xe Trung tâm Đà Nẵng</div>
+            <br />
             <div>Bến xe Gia Lâm</div>
+            <br />
             <div>Bến xe Mỹ Đình</div>
+            <br />
             <div>Bến xe An Sương</div>
+            <br />
             <div>Bến xe Nước Ngầm</div>
+            <br />
             <div>Bến xe Miền Tây</div>
           </div>
 
-          <div className={classes.Station}>
-            <div className={classes.titleStation}>Nhà xe</div>
+          <div className={classes.Garage}>
+            <div className={classes.titleGarage}>Nhà xe</div>
+            <br />
             <div>Xe Sao Việt</div>
+            <br />
             <div>Xe Taxi Hoa Mai</div>
+            <br />
             <div>Xe Hạ Long Travel</div>
+            <br />
             <div>Xe Quốc Đạt</div>
+            <br />
             <div>Xe Thanh Bình Xanh</div>
+            <br />
             <div>Xe Thiện Thành limousine</div>
+            <br />
             <div>Xe Hồng Sơn Phú Yên</div>
+            <br />
             <div>Xe Tiến Oanh</div>
           </div>
 
           <div className={classes.Station1}>
-            <div className={classes.titleStation1}></div>
+            <div className={classes.titleStation1}>&nbsp;</div>
+            <br />
             <div>Xe Hải Âu</div>
+            <br />
             <div>Xe Queen Cafe</div>
+            <br />
             <div>Xe Hưng Long</div>
+            <br />
             <div>Xe Xuân Tráng Limousine</div>
+            <br />
             <div>Xe Nam Á Châu</div>
+            <br />
             <div>Xe Khanh Phong</div>
+            <br />
             <div>Xe An Anh (Quê Hương)</div>
+            <br />
             <div>Xe Minh Quốc</div>
           </div>
 
           <div className={classes.Station2}>
-            <div className={classes.titleStation2}></div>
+            <div className={classes.titleStation2}>&nbsp;</div>
+            <br />
             <div>Xe Văn Minh</div>
+            <br />
             <div>Xe Quang Nghị</div>
+            <br />
             <div>Xe Xuân Trường</div>
+            <br />
             <div>Xe Hạnh Cafe</div>
+            <br />
             <div>Xe Trọng Minh</div>
+            <br />
             <div>Xe Ngọc Ánh Sài Gòn</div>
+            <br />
             <div>Xe Hoa Mai</div>
+            <br />
             <div>Xe Thuận Tiến</div>
           </div>
         </div>
         <div className={classes.bar3}>
           <div className={classes.About}>
             <div className={classes.titleAbout}>Về chúng tôi</div>
+            <br />
             <div>Phần mềm đại lý</div>
+            <br />
             <div>Giới Thiệu GrabStar.com</div>
+            <br />
             <div>Tuyển dụng</div>
+            <br />
             <div>Tin tức</div>
+            <br />
             <div>Liên hệ</div>
           </div>
 
           <div className={classes.Support}>
             <div className={classes.titleSupport}>Hỗ trợ</div>
+            <br />
             <div>Hướng dẫn thanh toán</div>
+            <br />
             <div>Quy chế GrabStar.com</div>
+            <br />
             <div>Chính sách bảo mật thông tin</div>
+            <br />
             <div>Chính sách bảo vệ thanh toán</div>
+            <br />
             <div>Chính sách và quy trình giải quyết tranh chấp, khiếu nại</div>
+            <br />
             <div>Câu hỏi thường gặp</div>
+            <br />
             <div>Tra cứu đơn hàng</div>
           </div>
+
+          <div className={classes.Certified}>
+            <div className={classes.titleCertified}>Chứng nhận</div>
+            <br />
+            <img
+              className={classes.logo1}
+              src={require("../../image/certificate0.png")}
+              alt=""
+            />
+            <br />
+            <img
+              className={classes.logo2}
+              src={require("../../image/certificate1.png")}
+              alt=""
+            />
+            <br />
+            <img
+              className={classes.logo3}
+              src={require("../../image/dangkybocongthuong.png")}
+              alt=""
+            />
+            <br />
+            <img
+              className={classes.logo4}
+              src={require("../../image/dangkybocongthuong.png")}
+              alt=""
+            />
+          </div>
+
+          <div className={classes.Download}>
+            <div className={classes.titleDownload}>Tải ứng dụng GrabStar</div>
+            <img
+              className={classes.logo5}
+              src={require("../../image/appstore.png")}
+              alt=""
+            />
+            <img
+              className={classes.logo6}
+              src={require("../../image/googleplay.png")}
+              alt=""
+            />
+          </div>
         </div>
-        <div className={classes.bar4}></div>
+        <br />
+        <br />
+        <br />
+        <div className={classes.div1}>&nbsp;</div>
+        <div className={classes.div2}>&nbsp;</div>
+        <div className={classes.div3}>&nbsp;</div>
+        <div className={classes.bar4}>
+          <div className={classes.License}>
+            <div className={classes.titleLicense}>
+              Công ty TNHH Thương Mại Dịch Vụ GrabStar
+            </div>
+            <div>
+              Địa chỉ đăng ký kinh doanh: 8C Chữ Đồng Tử, Phường 7, Quận Tân
+              Bình, Thành Phố Hồ Chí Minh, Việt Nam
+            </div>
+            <div>
+              Địa chỉ: Lầu 8 Tòa nhà CirCO, 222 Điện Biên Phủ, Quận 3, TP. Hồ
+              Chí Minh, Việt Nam
+            </div>
+            <div>
+              Giấy chứng nhận ĐKKD số 0315133726 do Sở KH và ĐT TP. Hồ Chí Minh
+              cấp lần đầu ngày 27/6/2018
+            </div>
+            <div>Bản quyền © 2022 thuộc về GrabStar.Com</div>
+          </div>
+        </div>
       </div>
     </main>
   );
